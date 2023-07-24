c421d9fd1deec8376eedcf49a694e9589fbf68c9
rebeccallll <liruijia0904@gmail.com>
2023-07-24 16:05:55 -0400

    Basic structure

---
+++ resume.js
@@ -0,0 +1,11 @@
+// Function to show the download modal
+function showDownloadWindow() {
+  const modal = document.getElementById('downloadModal');
+  modal.style.display = 'block';
+}
+
+// Function to close the download modal
+function closeDownloadWindow() {
+  const modal = document.getElementById('downloadModal');
+  modal.style.display = 'none';
+}
